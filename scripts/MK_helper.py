c7b1c06ecd953b4f635a2ffafad9fb9ccac6b34c
Sean Bittner <srb2201@columbia.edu>
2018-04-28 17:51:48 +0000

    random seed in filename

--- scripts/MK_helper.py
+++ scripts/MK_helper.py
@@ -11,16 +11,19 @@ K_eta = int(sys.argv[3]);
 M_eta = int(sys.argv[4]);
 flow_id = str(sys.argv[5]);
 cost_type = 'KL';
-ncons = D+1;
+if (exp_fam == 'normal'):
+  ncons = int(D + D*(D+1)/2);
+elif (exp_fam == 'dirichlet'):
+  ncons = D;
 L_theta = int(sys.argv[6]);
 upl_fac = int(sys.argv[7]);
 upl_theta = upl_fac*ncons;
 stochastic_eta = False;
 lr_order = -3;
 theta_nn_hps = {'L':L_theta, 'upl':upl_theta};
-max_iters = 20000;
+max_iters = 50000;
 check_rate = 200;
-random_seed = 0;
+random_seed = int(sys.argv[8]);
 
 X, trainKLs, it = train_efn(exp_fam, D, flow_id, cost_type, K_eta, M_eta, \
 			                                 stochastic_eta, theta_nn_hps, lr_order, \
