fc4b7f2930a1d0fc14954c9f6d487e6551595729
Sean Bittner <sbittner@seans-mbp.localdomain>
2018-09-28 17:37:05 +0200

    efn1 helper

--- scripts/train_efn1_helper.py
+++ scripts/train_efn1_helper.py
@@ -2,7 +2,7 @@ from train_efn import train_efn
 import numpy as np
 from scipy.stats import multivariate_normal
 import os, sys
-from families import family_from_str
+from tf_util.families import family_from_str
 from efn_util import model_opt_hps
 
 
@@ -15,10 +15,11 @@ dist_seed = int(sys.argv[4]);
 random_seed = int(sys.argv[5]);
 dir_str = str(sys.argv[6]);
 
-TIF_flow_type, nlayers, lr_order = model_opt_hps(exp_fam, D);
+TIF_flow_type, nlayers, scale_layer, lr_order = model_opt_hps(exp_fam, D);
 
 
 flow_dict = {'latent_dynamics':None, \
+			 'scale_layer':scale_layer, \
              'TIF_flow_type':TIF_flow_type, \
              'repeats':nlayers};
 
